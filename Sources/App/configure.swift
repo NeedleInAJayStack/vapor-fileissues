19938d6a1e0161435af497efed058834b5e89690
Jay Herron <NeedleInAJayStack@protonmail.com>
2023-01-24 17:57:46 -0700

    feature: Adds compression

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,5 +3,6 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) throws {
     app.http.server.configuration.hostname = "0.0.0.0"
+    app.http.server.configuration.responseCompression = .enabled
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory, defaultFile: "index.html"))
 }
