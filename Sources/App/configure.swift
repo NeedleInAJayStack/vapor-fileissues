0f06d2af20274f83b9e35ae304385890fe300e19
Jay Herron <NeedleInAJayStack@protonmail.com>
2023-01-24 17:57:18 -0700

    feature: Configure as fileserver

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,9 +2,6 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    // register routes
-    try routes(app)
+    app.http.server.configuration.hostname = "0.0.0.0"
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory, defaultFile: "index.html"))
 }
